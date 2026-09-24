a974bcc32d5a88a2e444a6d2bbef284c982ef248
David Ugwunnamuchi <davidugwunnamuchi14@gmail.com>
2026-09-23 23:53:34 -0700

    Update Portfolio

--- script.js
+++ script.js
@@ -17,3 +17,30 @@ navLinks.forEach((link) => {
     menuToggle.textContent = "☰";
   });
 });
+
+const revealElements = document.querySelectorAll(
+  ".about-content, .skills-content, .project-content, .contact-content",
+);
+
+revealElements.forEach((element) => {
+  element.classList.add("reveal");
+});
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("active");
+      } else {
+        entry.target.classList.remove("active");
+      }
+    });
+  },
+  {
+    threshold: 0.2,
+  },
+);
+
+revealElements.forEach((element) => {
+  observer.observe(element);
+});
